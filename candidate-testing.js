fb82cf1562c7f16f1738422c7c23f7bc41636fde
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-05-15 12:35:44 -0500

    1.2c grading correction

--- candidate-testing.js
+++ candidate-testing.js
@@ -32,7 +32,7 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered 
   //the question correctly or incorrectly // 
- if (candidateAnswer.includes("Sally Ride")) {
+ if (candidateAnswer == "Sally Ride") {
   console.log("Correct!")
   } else {
   console.log("Sorry. That's incorrect.")
